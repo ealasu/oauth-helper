bbcdae7c8ec2112c899d460885b8b8b7e03a9559
Emanuel Alasu <emanuel.alasu@gmail.com>
2013-05-25 14:33:06 -0700

    add query checking

--- app.js
+++ app.js
@@ -8,16 +8,26 @@ app.use(express.logger());
 var eventEmitter = new events.EventEmitter();
 
 app.get('/auth', function(req, res) {
-  eventEmitter.emit(req.query.oauth_token, req.query);
-  res.send('sent oauth params');
+  var key = req.query.oauth_token;
+  if (!key) {
+    res.status(400).send('must specify oauth_token');
+  } else {
+    eventEmitter.emit(key, req.query);
+    res.send('sent oauth params');
+  }
 });
 
 app.get('/waitforauth', function(req, res) {
-  eventEmitter.once(req.query.oauth_token, function(query) {
-    console.log('event: '+req.query.oauth_token);
-    console.log('query: '+query);
-    res.send(query);
-  });
+  var key = req.query.oauth_token;
+  if (!key) {
+    res.status(400).send('must specify oauth_token');
+  } else {
+    eventEmitter.once(key, function(query) {
+      console.log('event: '+key);
+      console.log('query: '+query);
+      res.send(query);
+    });
+  }
 });
 
 var port = process.env.PORT || 5000;
